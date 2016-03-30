b857de29f1b89b17b8a72225b8cf1af0a84042bb
Alex Johnson <alex@Alexs-MacBook-Pro.local>
2016-03-30 12:31:35 -0600

    Working version for heat transfer above LAB

--- Honors_Thesis/orig.py
+++ Honors_Thesis/orig.py
@@ -1,8 +1,12 @@
+import numpy as np
 from time import clock
 from os import makedirs
 import math
-from dolfin import *
-
+from dolfin import ERROR, set_log_level, exp, near, File, Expression, tanh, \
+                   Constant, SubDomain, VectorFunctionSpace, FunctionSpace, \
+                   DirichletBC, Function, split, \
+                   MixedFunctionSpace, TestFunctions, inner, sym, grad, div, dx, \
+                   RectangleMesh, Point, solve, project, assign, interpolate
 
 '''
 This version of the code runs a swarm of simulations of various viscosities
@@ -29,6 +33,7 @@ theta = 0.5
 h = 1000000.0
 kappa_0 = 1.E-6
 
+output_every = 100
 nx = 20
 ny = 20
 # non-dimensional mesh size
@@ -68,6 +73,7 @@ def RunJob(Tb, mu_value, path):
     parameters = open(path + '/parameters', 'w', 0)
     vmeltfile = File(path + '/vmelt.pvd')
     rhofile = File(path + '/rhosolid.pvd')
+    fluidTemp = File(path + '/fluid_temp.pvd')
 
     for name in dir():
         ev = str(eval(name))
@@ -79,7 +85,7 @@ def RunJob(Tb, mu_value, path):
     temp_values = [x / dTemp for x in temp_values]  # non dimensionalising temp
 
     mu_a = mu_value  # this was taken from the blankenbach paper, can change
-    
+
     Ep = b / dTemp
 
     mu_bot = exp(-Ep * (temp_values[3] * dTemp - 1573) + cc) * mu_a
@@ -96,7 +102,7 @@ def RunJob(Tb, mu_value, path):
     noslip = Constant((0.0, 0.0))
 
     dt = 3.E11 / tau
-    tEnd = 3.E13 / tau  # non-dimensionalising times
+    tEnd = 3.E15 / tau  # non-dimensionalising times
 
     class PeriodicBoundary(SubDomain):
         def inside(self, x, on_boundary):
@@ -111,15 +117,26 @@ def RunJob(Tb, mu_value, path):
     class TempExp(Expression):
         def eval(self, value, x):
             if x[1] >= LAB(x):
-                value[0] = temp_values[0] + (temp_values[1] - temp_values[0]) * (MeshHeight - x[1]) / (MeshHeight - LAB(x))
+                value[0] = temp_values[0] + \
+                    (temp_values[1] - temp_values[0]) * (MeshHeight - x[1]) / (MeshHeight - LAB(x))
             else:
                 value[0] = temp_values[3] - (temp_values[3] - temp_values[2]) * (x[1]) / (LAB(x))
 
     class FluidTemp(Expression):
-        def eval(self, value, x):
-            if value[0] < 1295:
-                value[0] = 1295
+        def __init__(self, P):
+            self.T0 = T0
 
+        def eval(self, value, x):
+            t_val = np.zeros(1, dtype='d')
+            self.T0.eval(t_val, x)
+
+            LAB_temp = 1.013
+            # value[0] = min(
+            if t_val <= LAB_temp:
+                value[0] = LAB_temp
+            else:
+                value[0] = t_val
+    
     mesh = RectangleMesh(Point(0.0, 0.0), Point(MeshWidth, MeshHeight), nx, ny)
 
     Svel = VectorFunctionSpace(mesh, 'CG', 2, constrained_domain=pbc)
@@ -132,11 +149,15 @@ def RunJob(Tb, mu_value, path):
 
     u = Function(S0)
     v, p, T = split(u)
+    # v, p, T = u.split()
+
+    # print(type(v), v)
+    # print(type(u.sub(0)), u.sub(0))
     v_t, p_t, T_t = TestFunctions(S0)
 
     T0 = interpolate(TempExp(), Stemp)
 
-    muExp = Expression('exp(-Ep * (T_val * dTemp - 1573) + cc * x[2] / meshHeight)', Smu.ufl_element(),
+    muExp = Expression('exp(-Ep * (T_val * dTemp - 1573) + cc * x[2] / meshHeight)',
                         Ep=Ep, dTemp=dTemp, cc=cc, meshHeight=MeshHeight, T_val=T0)
 
     mu = interpolate(muExp, Smu)
@@ -147,6 +168,8 @@ def RunJob(Tb, mu_value, path):
     v0 = Function(Svel)
     vmelt = Function(Svel)
 
+    Tf = Function(Stemp)
+    
     v_theta = (1. - theta)*v0 + theta*v
 
     T_theta = (1. - theta)*T + theta*T0
@@ -157,29 +180,13 @@ def RunJob(Tb, mu_value, path):
 
     r_p = p_t*div(v)*dx
 
+    k_s = Constant(1.0E-6)
+
     r_T = (T_t*((T - T0) \
         + dt*inner(v_theta, grad(T_theta))) \
-        + (dt/Ra)*inner(grad(T_t), grad(T_theta)) )*dx
-#           + k_s*(Tf-T_theta)*dt
+        + (dt/Ra)*inner(grad(T_t), grad(T_theta))
+        + T_t * k_s * (Tf-T_theta) * dt) * dx
 
-    Tf = Function(Stemp)
-    Tf.assign(T0)
-    Tf.interpolate(FluidTemp())
-    # Tf = T0.interpolate(Expression('value[0] >= 1295.0 ? value[0] : 1295.0'))
-
-    # Tf.interpolate(Expression('value[0] >= 1295 ? value[0] : 1295'))
-    # project(Expression('value[0] >= 1295 ? value[0] : 1295'), Tf)
-# Alex, a question for you:
-# can you see if there is a way to set Tf = T in regions where T >=1295 celsius
-#
-# 1295 celsius is my arbitrary choice for the LAB isotherm.  In regions
-# where T < 1295 C, set Tf to be some constant for now, such as 1295 C.
-# Once we do this, then we can add in a term like that last line above where
-# it will only be non-zero when the solid temperature, T, is cooler than 1295
-# can you do this? After this is done, we will then worry about a calculation
-# where we solve for Tf as a function of time in the regions cooler than 1295 C
-# Makes sense?  If not, we can skype soon -- email me with questions
-# 3/19/16
     r = r_v + r_p + r_T
 
     bcv0 = DirichletBC(S0.sub(0), noslip, top)
@@ -193,15 +200,20 @@ def RunJob(Tb, mu_value, path):
     t = 0
     count = 0
     while (t < tEnd):
+        # assign(Tf, T0)
+        Tf.interpolate(FluidTemp(T0))
+
         solve(r == 0, u, bcs)
-        t += dt
         nV, nP, nT = u.split()
+        
         gp = grad(nP)
         rhosolid = rho_0 * (1 - alpha * (nT * dTemp - 1573))
         deltarho = rhosolid - rhomelt
         yvec = Constant((0.0, 1.0))
         vmelt = nV * w0 - darcy * (gp * p0 / h - deltarho * yvec * g)
-        if (count % 100 == 0):
+        
+        if count % output_every == 0:
+            fluidTemp << Tf
             pfile << nP
             ufile << nV
             tfile << nT
@@ -210,18 +222,19 @@ def RunJob(Tb, mu_value, path):
             mufile << project(mu * mu_a, Smu)
             rhofile << project(rhosolid, Srho)
             vmeltfile << project(vmelt, Svel)
-        count += 1
+
         assign(T0, nT)
         assign(v0, nV)
         mu.interpolate(muExp)
-        print('Success')
 
+        print(t)
+        t += dt
+        count += 1
+        
     print('Case mu=%g, Tb=%g complete.' % (mu_a, Tb), ' Run time =', clock() - runtimeInit, 's')
 
 if __name__ == "__main__":
     Tbs = [1300]
-    #Tbs = [800]
-    #Mus = numpy.linspace(1e19,1e21,5)
     Mus = [5e21]
     for mu in Mus:
         mufolder = 'mu=' + str(mu)
